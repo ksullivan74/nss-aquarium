d9894634950e763da721d318cde475c5eebb8abb
Neil Sullivan <ac30delay@gmail.com>
2022-12-06 15:41:10 -0600

    update 12.06.22

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+const fish = 
+[
+    {
+        fishName: "Clown Fish",
+        species: "",
+        length: "",
+        whereHarvested: "",
+        whatItEats: ""
+    },
+    {
+        fishName: "Damselfish",
+        species: "",
+        length: "",
+        whereHarvested: "",
+        whatItEats: ""
+    },
+    {
+        fishName: "Bicolor Blenny",
+        species: "",
+        length: "",
+        whereHarvested: "",
+        whatItEats: ""
+    },
+    {
+        fishName: "Hawkfish",
+        species: "",
+        length: "",
+        whereHarvested: "",
+        whatItEats: ""
+    }
+]
+
